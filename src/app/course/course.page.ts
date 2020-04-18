01a24045972165a7d1a7818454f67e0a00574205
khl2007 <khalilqandil@gmail.com>
2020-04-18 05:12:06 +0300

    Update course.page.ts

--- src/app/course/course.page.ts
+++ src/app/course/course.page.ts
@@ -51,5 +51,55 @@ let data = {
 
 this.discussMsg='';
 }
+  
+
+openImagePicker(){
+    this.imagePicker.hasReadPermission()
+    .then((result) => {
+      if(result == false){
+        // no callbacks required as this opens a popup which returns async
+        this.imagePicker.requestReadPermission();
+      }
+      else if(result == true){
+        this.imagePicker.getPictures({
+          maximumImagesCount: 1,
+          width: 800,
+          height: 800,
+        }).then(
+          (results) => {
+            for (var i = 0; i < results.length; i++) {
+              this.uploadImageToFirebase(results[i]);
+            }
+          }, (err) => console.log(err)
+        );
+      }
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+  async uploadImageToFirebase(image){
+    const loading = await this.loadingCtrl.create({
+      message: 'Please wait...'
+    });
+    const toast = await this.toastCtrl.create({
+      message: 'Image was updated successfully',
+      duration: 3000
+    });
+    this.presentLoading(loading);
+    let image_src = this.webview.convertFileSrc(image);
+    let randomId = Math.random().toString(36).substr(2, 5);
+
+    //uploads img to firebase storage
+    this.firebaseService.uploadImage(image_src, randomId)
+    .then(photoURL => {
+      this.image = photoURL;
+      loading.dismiss();
+      toast.present();
+    }, err =>{
+      console.log(err);
+    })
+  }
+
 
 }
