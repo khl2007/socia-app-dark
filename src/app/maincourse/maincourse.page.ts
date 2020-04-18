939e56ecad348170bf4c59b089e476f2f75cc03c
khl2007 <khalilqandil@gmail.com>
2020-04-18 08:39:00 -0700

    1 file modified, 5 added

---
+++ src/app/maincourse/maincourse.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-maincourse',
+  templateUrl: './maincourse.page.html',
+  styleUrls: ['./maincourse.page.scss'],
+})
+export class MaincoursePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
