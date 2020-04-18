743cdd64b1cfbc95d08ab32b80dc64c8bf84e012
khl2007 <khalilqandil@gmail.com>
2020-04-18 22:24:29 +0300

    Update maincourse-routing.module.ts

--- src/app/maincourse/maincourse-routing.module.ts
+++ src/app/maincourse/maincourse-routing.module.ts
@@ -1,1 +1,18 @@
 
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { MaincoursePage } from './Maincourse.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MaincoursePage
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class MaincoursePageRoutingModule {}
