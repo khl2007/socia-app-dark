e09f58c741738dd7be1aa2d53ecf1b1e9e9e2476
khl2007 <khalilqandil@gmail.com>
2020-04-18 08:37:26 -0700

    3 files added

---
+++ src/app/comps/userfolowers/userfolowers.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-userfolowers',
+  templateUrl: './userfolowers.component.html',
+  styleUrls: ['./userfolowers.component.scss'],
+})
+export class UserfolowersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
