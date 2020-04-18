528ec1aaa5924d0863c3fbca4f8787ea47214b42
khl2007 <khalilqandil@gmail.com>
2020-04-18 22:37:05 +0300

    Update maincourse-routing.module.ts

--- src/app/maincourse/maincourse-routing.module.ts
+++ src/app/maincourse/maincourse-routing.module.ts
@@ -2,7 +2,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { MaincoursePage } from './Maincourse.page';
+import { MaincoursePage } from './maincourse.page';
 
 const routes: Routes = [
   {
