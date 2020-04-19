af1bc75ee2687cd28151695692905b47ec46b559
khl2007 <khalilqandil@gmail.com>
2020-04-20 00:59:10 +0300

    Update userfolowers.component.ts

--- src/app/comps/userfolowers/userfolowers.component.ts
+++ src/app/comps/userfolowers/userfolowers.component.ts
@@ -1,4 +1,20 @@
-import { Component, OnInit , Input } from '@angular/core';
+import { Component, OnInit , ViewChild , Input} from "@angular/core";
+import { AuthService } from "../../services/auth.service";
+import { LoadingController } from "@ionic/angular";
+import { Router, ActivatedRoute } from "@angular/router";
+
+//import { Animation, AnimationController  } from '@ionic/angular';
+
+//import { IonInfiniteScroll } from '@ionic/angular';
+
+import { Observable } from "rxjs";
+
+import { map } from "rxjs/operators";
+
+import { FollowService } from "../../services/follow.service";
+
+import { FirebaseService } from "../../services/firebase.service";
+
 
 @Component({
   selector: 'app-userfolowers',
@@ -8,11 +24,64 @@ import { Component, OnInit , Input } from '@angular/core';
 export class UserfolowersComponent implements OnInit {
     
 
-@Input() users: any;
+@Input() uid: string;
+  @Input() type: string;
+  @Input() modalRef;
 
+  //username;
+  displayname;
+  photoURL;
+  status;
 
-  constructor() { }
+  isCurrentUser = false;
 
-  ngOnInit() {}
+  btnFollow = 'Follow';
+
+  constructor(
+    public loadingCtrl: LoadingController,
+    private auth: AuthService,
+    private route: ActivatedRoute,
+    private userService: FirebaseService,
+    private followService: FollowService
+  ) { }
+
+  ngOnInit() {
+    this.userService.getUserInfo(this.uid).subscribe(
+      user => {
+        if (user) {
+          this.username = user.userName;
+          this.displayname = user.displayName;
+          //this.status = user.status;
+          this.photoURL = user.avatar;
+          this.checkFollowing();
+        }
+    });
+  }
+
+  checkFollowing() {
+    this.auth.getAuthState().subscribe(user => {
+      if (user) {
+        if (user.uid === this.uid) {
+          this.isCurrentUser = true;
+        } else {
+          this.followService.isFollowing(this.uid, user.uid).subscribe(followinguser => {
+            if (followinguser.length > 0) {
+              this.btnFollow = 'Following';
+            } else {
+              this.btnFollow = 'Follow';
+            }
+          });
+        }
+      }
+    });
+  }
+
+  follow() {
+    if (this.btnFollow === 'Following') {
+      this.followService.unfollow(this.uid);
+    } else {
+      this.followService.follow(this.uid);
+    }
+  }
 
 }
