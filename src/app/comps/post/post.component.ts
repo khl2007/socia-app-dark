c3b7e06fc0bf8c4ebce384d19d63cda894744a6f
khl2007 <khalilqandil@gmail.com>
2020-04-19 21:54:08 +0300

    Update post.component.ts

--- src/app/comps/post/post.component.ts
+++ src/app/comps/post/post.component.ts
@@ -11,6 +11,42 @@ export class PostComponent implements OnInit {
   @Input() inputPost;
   @Input() inputPostID;
   @Input() parentPid;
+  
+
+  currentuser;
+  currentuid;
+  showContext = true;
+
+  isLoggedIn = false;
+  isSingle = false;
+  isCurrentUser = false;
+  isInvalid;
+  isLoaded = false;
+  showLoader = false;
+  closeResult: string;
+  isLiked;
+  likeStyle = 'fa fa-thumbs-o-up';
+  likeLen = 0;
+  commentLen = 0;
+
+  pid;
+  displayName;
+  userName;
+  photoURL = '../../assets/images/default-profile.jpg';
+  body;
+  date;
+  likes;
+  type;
+  comments;
+  postPhotoURL;
+
+  parentUsername;
+  parentUID;
+
+  gname;
+  gid;
+
+
 
 
   constructor() { }
